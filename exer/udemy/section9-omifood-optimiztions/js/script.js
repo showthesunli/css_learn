ba82f117390dc7a7e4fe404c0eaef9e5b285581b
showthesunli <showthesunli@gmail.com>
2022-06-08 04:55:54 +0800

    imporve

--- exer/udemy/section9-omifood-optimiztions/js/script.js
+++ exer/udemy/section9-omifood-optimiztions/js/script.js
@@ -1,13 +1,12 @@
 `use strict`
 
-let openIcon = document.querySelector('ion-icon[name=menu-outline]')
-let closeIcon = document.querySelector('ion-icon[name=close-outline]')
 let header = document.querySelector('header.header')
 
 function addMobileNavListener(){
     
-    openIcon.addEventListener("click", ()=>header.classList.add('open-nav'))
-    closeIcon.addEventListener("click", ()=>header.classList.remove('open-nav'))
+    header.addEventListener("click", ()=>{
+        header.classList.toggle('open-nav')
+    })
 }
 
 
